76d15169ad1db0c99f46a4c36dc38ec59201cf88
José Santos <josemmsantos14@gmail.com>
2023-04-13 02:04:28 +0100

    added tpc6

---
+++ tpc6/README.md
@@ -0,0 +1,5 @@
+Para o desenvolvimento da página web incluindo os tópicos "engenharia", "elefante" e "égua", foram formulados três ficheiros. O primeiro diz respeito ao HTML, o segundo ao CSS e o terceiro ao JavaScript.
+
+O design idealizado vai de encontro à apresentação dos tópicos através de secções individuais, mais particularmente cartões. Dentro de cada um os elementos encontram-se dispostos pelos mesmos príncipios.
+
+Algumas funcionalidades para interação dinâmica com o utilizador foram implementadas através do CSS, como os efeitos de zoom na imagem, e outros através da linguagem de programação JavaScript, como a apresentação ou não das designações.
---
+++ tpc6/app.js
@@ -0,0 +1,42 @@
+function showEng() {
+  var x = document.getElementById("eng-desc");
+  var y = document.getElementById("eng-pic");
+  var z = document.getElementById("eng-btn");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.marginTop = "1rem";
+    z.text = "Less";
+  } else {
+    x.style.display = "none";
+    y.style.marginTop = "8rem";
+    z.text = "More";
+  }
+}
+function showElef() {
+  var x = document.getElementById("elef-desc");
+  var y = document.getElementById("elef-pic");
+  var z = document.getElementById("elef-btn");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.marginTop = "1rem";
+    z.text = "Less";
+  } else {
+    x.style.display = "none";
+    y.style.marginTop = "8rem";
+    z.text = "More";
+  }
+}
+function showHorse() {
+  var x = document.getElementById("egua-desc");
+  var y = document.getElementById("egua-pic");
+  var z = document.getElementById("egua-btn");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.marginTop = "1rem";
+    z.text = "Less";
+  } else {
+    x.style.display = "none";
+    y.style.marginTop = "8rem";
+    z.text = "More";
+  }
+}
